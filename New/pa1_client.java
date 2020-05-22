0a1888984b03bbb2d37fafc858a18b6ac7875c5b
RightEdCha <x64marks@ufl.edu>
2020-05-22 17:05:39 -0400

    Reworked Client Changes

--- New/pa1_client.java
+++ New/pa1_client.java
@@ -1,57 +1,134 @@
 import java.io.*;
 import java.net.*;
 
-class client{
-    static Boolean clientTerminated = false;
+class ClientTCPConnectionResources{
+    //Deals with creating, saving and deleting TCP connection resources.
+    private String serverName;
+    private int port;
+    private Socket socket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
 
-    public static void main(String argv[]) throws Exception{
-        String clientCommand;
-        String serverResponse;
-        String hostName = argv[0];
-        int port = Integer.parseInt(argv[1]);
-
-        //Set up socket and attach a buffer to read from and write to.
-        Socket socket = new Socket(hostName, port);
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-
-        //Read the inital hello message.
-        serverResponse = inFromServer.readLine();
-        printToClientTerminalBasedOnServerResponse(serverResponse);
-
-        //Keep connection persistent from client side.
-        while(!clientTerminated){
-            clientCommand = userInput.readLine();
-            outToServer.flush();
-            outToServer.writeBytes(clientCommand + "\n");
-            serverResponse = inFromServer.readLine();
-            printToClientTerminalBasedOnServerResponse(serverResponse);
+    public ClientTCPConnectionResources(String serverName, int port){
+        this.serverName = serverName;
+        this.port = port;
+        try{
+            Socket socket = new Socket(serverName, port);
+            this.inputStream = socket.getInputStream();
+            this.outputStream = socket.getOutputStream();
         }
-        socket.close();
+        catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
     }
 
-    private static void printToClientTerminalBasedOnServerResponse(String response){
-        String initialString = "receive: ";
+    public String getServerName() {
+        return serverName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public OutputStream getOutputStream() {
+        return outputStream;
+    }
+
+    public String toString(){
+        return "Connected To: " + serverName + " On Port: " + port;
+    }
+
+    public void closeResources() {
+        try{
+            socket.close();
+        }
+        catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
+
+class StringTCPHandler{
+    //Handles sending and receiving information as strings.
+    public void sendData(String data, OutputStream outputStream){
+        PrintWriter writer = new PrintWriter(outputStream, true);
+        writer.println(data);
+    }
+
+    public String readData(InputStream inputStream){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String output = "";
+        try{
+            output = reader.readLine(); 
+        }
+        catch(Exception e){
+            return "";
+        }
+        return output;
+    }
+}
+
+class DataConvertor {
+    //Specific conversion of output
+    public String toString(String response){
         if(response.equals("-1")){
-            System.out.println(initialString + "incorrect operation command.");
+            return "incorrect operation command.";
         }
         else if(response.equals("-2")){
-            System.out.println(initialString + "number of inputs is less than two.");
+            return "number of inputs is less than two.";
         }
         else if(response.equals("-3")){
-            System.out.println(initialString + "number of inputs is more than four.");
+            return "number of inputs is more than four.";
         }
         else if(response.equals("-4")){
-            System.out.println(initialString + "one or more of the inputs contain(s) non-number(s).");
+            return "one or more of the inputs contain(s) non-number(s).";
         }
-        else if(response.equals("-5")){ 
-            clientTerminated = true; 
-            System.out.println(initialString + "exit.");
+        else if(response.equals("-5")){
+            return "exit.";
         } 
         else { 
-            System.out.println(initialString + response); 
+            return response;
         }
     }
+}
 
+class ClientApplication{
+    //Runs the main application along with handling user inputs and printing to console.
+    public static void main(String argv[]) throws Exception{
+        String serverName = argv[0];
+        int port = Integer.parseInt(argv[1]);
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        try{
+            ClientTCPConnectionResources TCPConnectionResources = new ClientTCPConnectionResources(serverName, port);
+            System.out.println(TCPConnectionResources);
+            StringTCPHandler handler = new StringTCPHandler();
+            DataConvertor convertor = new DataConvertor();
+
+            System.out.println(handler.readData(TCPConnectionResources.getInputStream()));
+
+            while(true){
+                handler.sendData(userInput.readLine(), TCPConnectionResources.getOutputStream());
+                String response = handler.readData(TCPConnectionResources.getInputStream());
+                System.out.println(convertor.toString(response));
+
+                if(response.equals("-5")){
+                    break;
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
